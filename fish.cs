279b6f65cd40836114ef6a9f3694bebba626342f
Alexander <alex.esbj@gmail.com>
2019-09-30 14:00:35 +0200

    börjar på del 1
    gjort allt utom testobjekt(?)

---
+++ fish.cs
@@ -0,0 +1,15 @@
+using System;
+namespace excercises_classes{
+    public class Fish
+    {
+        string name;
+        string race;
+        int age;
+        public static void Trick(){
+            Console.WriteLine("...");
+        }
+        public static void Speak(){
+            Console.WriteLine("...");
+        }
+    }
+}
